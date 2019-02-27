792e901a0b977fb9b667bf061f8d685eea7f98b9
Mark Drummond <dmark@users.noreply.github.com>
2019-02-27 08:04:03 -0500

    Better handles missing 'roles' list.

--- rules_per_app.js
+++ rules_per_app.js
@@ -1,11 +1,13 @@
 // This rule provisions limited access to the Management API for authorized
-// users and applications. IF the audience is the Management API AND the
-// application (client) has "ManagementAPIAccess = True" in the application
-// metadata, AND the user has '{ "roles": ["ManagementAPIAccess"]' } in their
-// app_metadata, then the user is given the scopes listed in context.accessToken.scope.
+// users and applications.
 //
-// Replace YOUR_TENANT with the name of you tenant. Update context.accessToken.scope
-// as needed.
+//   IF the audience is the Management API
+//     AND the app has "ManagementAPIAccess = True" in application metadata,
+//     AND the user has '{ "roles": ["ManagementAPIAccess"]' } in app_metadata,
+//   THEN the user is given the scopes listed in context.accessToken.scope
+//
+// Replace YOUR_TENANT with the name of you tenant.
+// Update context.accessToken.scope as needed.
 function (user, context, callback) {
   var audience = '';
   audience = audience ||
@@ -13,7 +15,7 @@ function (user, context, callback) {
     (context.request && context.request.body && context.request.body.audience);
   if (audience === 'https://[YOUR_TENANT].auth0.com/api/v2/') {
     var client_authorized = context.clientMetadata && context.clientMetadata.ManagementAPIAccess === "True";
-    user.app_metadata = user.app_metadata || {};
+    user.app_metadata.roles = user.app_metadata.roles || [];
     var user_authorized = user.app_metadata.roles.indexOf('ManagementAPIAccess') !== -1;
     if (client_authorized && user_authorized) {
       context.accessToken.scope = "read:rules read:clients";
